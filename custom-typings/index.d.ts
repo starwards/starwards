5ff17745e6fc8603d629ceca1a938db91ee24d41
Amir Arad <greenshade@gmail.com>
2019-05-02 22:35:02 +0300

    fix build and remove gql

--- custom-typings/index.d.ts
+++ custom-typings/index.d.ts
@@ -2,5 +2,5 @@ declare module '@colyseus/monitor' {
     // import express from 'express';
     // import { Server } from "colyseus";
     // export declare function monitor(server: Server): express.Router;
-    export declare function monitor(server: any): any;
+    export function monitor(server: any): any;
 }
--- modules/gql-server/test/tools/resolvers.ts
+++
@@ -1,9 +0,0 @@
-import { readSchema } from '../../schema/reader';
-import { makeExecutableSchema } from 'graphql-tools';
-// the actual resolvers
-import { resolvers } from '@starwards/server';
-
-// reading the actual schema
-export const typeDefs = readSchema('api');
-// make the actual schema and resolvers executable
-export const schema = makeExecutableSchema({ typeDefs, resolvers });
