9b4f80095bd0c385ec821b2b4dba5eeb6a3c5b35
Amir Arad <greenshade@gmail.com>
2019-06-07 18:01:50 +0300

    change map to big bang

--- modules/server/src/space/map.ts
+++ modules/server/src/space/map.ts
@@ -1,26 +1,23 @@
-
 import { Asteroid, SpaceObject } from '@starwards/model';
 import { Vec2 } from '@starwards/model';
 import nanoid from 'nanoid';
 import { Spaceship } from '@starwards/model';
 
-const fieldSize = 10000;
 const speedMax = 50;
 const asteroidSize = 30;
 export const shipId = 'shippy mcshipface';
 
-export const map = Array(1000).fill(null).map<SpaceObject>(() => new Asteroid(
-    nanoid(),
-    new Vec2( Math.random() * fieldSize - fieldSize / 2, Math.random() * fieldSize - fieldSize / 2),
-    Math.random() * asteroidSize
-));
-map.forEach(o =>
-    o.velocity = new Vec2( (Math.random() * speedMax * 2) - speedMax, (Math.random() * speedMax * 2) - speedMax));
-const ship = new Spaceship(
-    shipId,
-    new Vec2( 0, 0),
-    10
+export const map = Array(1000)
+  .fill(null)
+  .map<SpaceObject>(
+    () => new Asteroid(nanoid(), new Vec2(0, 0), Math.random() * asteroidSize)
+  );
+map.forEach(
+  o => {
+    o.velocity = Vec2.Rotate({ x: Math.random() * speedMax, y: 0 }, (Math.random() * 360 * 10) % 360);
+  }
 );
+const ship = new Spaceship(shipId, new Vec2(0, 0), 10);
 ship.angle = Math.random() * 360 * 10;
-ship.velocity = Vec2.Rotate({x : speedMax / 2, y : 0}, ship.angle);
+ship.velocity = Vec2.Rotate({ x: speedMax / 2, y: 0 }, ship.angle);
 map.push(ship);
