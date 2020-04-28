4f19341f4db230bd1ff76ecc9e9097fc4546e590
Amir Arad <greenshade@gmail.com>
2020-04-28 19:33:31 +0300

    add piloting widjet

--- modules/browser/src/screens/player.ts
+++ modules/browser/src/screens/player.ts
@@ -2,6 +2,7 @@ import $ from 'jquery';
 import { radarWidget } from '../widgets/radar';
 import { Dashboard } from '../widgets/dashboard';
 import { keyboardWidget } from '../widgets/keyboard';
+import { pilotWidget } from '../widgets/pilot';
 
 const dashboard = new Dashboard({ content: [] }, $('#layoutContainer'));
 dashboard.setup();
@@ -11,6 +12,7 @@ const shipId = urlParams.get('id');
 if (shipId) {
     dashboard.registerWidget(radarWidget, { subjectId: shipId });
     dashboard.registerWidget(keyboardWidget, { shipId });
+    dashboard.registerWidget(pilotWidget, { shipId });
 } else {
     // tslint:disable-next-line: no-console
     console.log('shipId is missing from URL');
