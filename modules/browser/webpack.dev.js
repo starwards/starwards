81c445644f0d3b3c06f3bc428020f58e2aa8f20a
Amir Arad <greenshade@gmail.com>
2020-05-21 02:00:33 +0300

    debug browser from vscode

--- modules/browser/webpack.dev.js
+++ modules/browser/webpack.dev.js
@@ -4,9 +4,11 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: 'development',
+    devtool: 'cheap-module-eval-source-map',
     entry: {
-        main: ['webpack-hot-middleware/client?reload=true'],
+        index: ['webpack-hot-middleware/client?reload=true'],
         player: ['webpack-hot-middleware/client?reload=true'],
+        gm: ['webpack-hot-middleware/client?reload=true'],
     },
     plugins: [new webpack.HotModuleReplacementPlugin()],
 });
