0b68a104da0252f67a5103dbcfd5f83bbb837d15
Amir Arad <greenshade@gmail.com>
2020-06-24 23:20:36 +0300

    add 2 grid magnitudes below sector

--- modules/browser/src/radar/grid-layer.ts
+++ modules/browser/src/radar/grid-layer.ts
@@ -4,8 +4,10 @@ import { PixiFps } from './pixi-fps';
 import { CameraView } from './camera-view';
 import { TextsPool } from './texts-pool';
 
+const scaleFactor = 8;
+const miniSectorSize = sectorSize / (scaleFactor * 2);
 export class GridLayer {
-    private static readonly gridColors = [0x6666ff, 0xf4fa77, 0x55ff55, 0xff3333];
+    private static readonly gridColors = [0xcccccc, 0xcccccc, 0x6666ff, 0xf4fa77, 0x55ff55, 0xff3333];
     private stage = new PIXI.Container();
     private fpsCounter = new PixiFps();
 
@@ -25,8 +27,8 @@ export class GridLayer {
 
     private drawSectorGrid() {
         this.gridLines.clear();
-        const minMagnitude = Math.max(0, Math.floor(Math.abs(Math.log10(this.parent.camera.zoom / 2.5)) - 2));
-        const minGridCellSize = sectorSize * Math.pow(8, minMagnitude);
+        const minMagnitude = Math.max(0, Math.floor(Math.abs(Math.log10(this.parent.camera.zoom))));
+        const minGridCellSize = miniSectorSize * Math.pow(scaleFactor, minMagnitude);
         const topLeft = this.parent.screenToWorld(XY.zero);
         const bottomRight = this.parent.screenToWorld({
             x: this.parent.renderer.width,
@@ -36,7 +38,7 @@ export class GridLayer {
         const horizontals = [];
         const gridlineHorizTop = topLeft.y - (topLeft.y % minGridCellSize);
         for (let world = gridlineHorizTop; world <= bottomRight.y; world += minGridCellSize) {
-            const distFrom0 = Math.abs(world) / sectorSize;
+            const distFrom0 = Math.abs(world) / miniSectorSize;
             const magnitude = this.calcGridLineMagnitude(minMagnitude, distFrom0);
             if (magnitude) {
                 const screen = this.parent.worldToScreen({ x: 0, y: world }).y;
@@ -47,7 +49,7 @@ export class GridLayer {
         }
         const gridlineVertLeft = topLeft.x - (topLeft.x % minGridCellSize);
         for (let world = gridlineVertLeft; world < bottomRight.x; world += minGridCellSize) {
-            const distFrom0 = Math.abs(world) / sectorSize;
+            const distFrom0 = Math.abs(world) / miniSectorSize;
             const magnitude = this.calcGridLineMagnitude(minMagnitude, distFrom0);
             if (magnitude) {
                 const screen = this.parent.worldToScreen({ x: world, y: 0 }).x;
@@ -58,16 +60,20 @@ export class GridLayer {
         }
         const textsIterator = this.sectorNames[Symbol.iterator]();
         for (const horizontal of horizontals) {
-            for (const vertical of verticals) {
-                const text = textsIterator.next().value;
-                const gridlineColor =
-                    horizontal.magnitude.scale < vertical.magnitude.scale
-                        ? horizontal.magnitude.color
-                        : vertical.magnitude.color;
-                text.text = getSectorName(horizontal.world, vertical.world);
-                text.style.fill = gridlineColor;
-                text.x = vertical.screen;
-                text.y = horizontal.screen;
+            if (horizontal.magnitude.scale > 1) {
+                for (const vertical of verticals) {
+                    if (vertical.magnitude.scale > 1) {
+                        const text = textsIterator.next().value;
+                        const gridlineColor =
+                            horizontal.magnitude.scale < vertical.magnitude.scale
+                                ? horizontal.magnitude.color
+                                : vertical.magnitude.color;
+                        text.text = getSectorName(horizontal.world, vertical.world);
+                        text.style.fill = gridlineColor;
+                        text.x = vertical.screen;
+                        text.y = horizontal.screen;
+                    }
+                }
             }
         }
         textsIterator.return();
@@ -75,7 +81,7 @@ export class GridLayer {
 
     private calcGridLineMagnitude(minMagnitude: number, position: number) {
         for (let i = GridLayer.gridColors.length - 1; i >= minMagnitude; i--) {
-            if (position % Math.pow(8, i) === 0) {
+            if (position % Math.pow(scaleFactor, i) === 0) {
                 const scale = Math.min(Math.floor(i), GridLayer.gridColors.length - 1);
                 return { scale, color: GridLayer.gridColors[scale] };
             }
