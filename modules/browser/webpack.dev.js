8cfd0a70b4d29ecad03efe1391bca7b70ffa460d
Amir Arad <greenshade@gmail.com>
2020-07-06 17:39:05 +0300

    fixed chrome debugging

--- modules/browser/webpack.dev.js
+++ modules/browser/webpack.dev.js
@@ -4,7 +4,7 @@ const common = require('./webpack.common.js');
 
 module.exports = merge(common, {
     mode: 'development',
-    devtool: 'cheap-module-eval-source-map',
+    devtool: 'source-map', //'cheap-module-eval-source-map',
     entry: {
         index: ['webpack-hot-middleware/client?reload=true'],
         player: ['webpack-hot-middleware/client?reload=true'],
