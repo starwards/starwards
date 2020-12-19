f21e62d97a40bdb2a74575c624e4131d3f3bc67a
Amir Arad <greenshade@gmail.com>
2020-12-19 11:19:52 +0200

    extract generic constructors from ship properties

---
+++ modules/model/src/logic/property-constructors.ts
@@ -0,0 +1,53 @@
+import { CommandName, RoomName, State } from '..';
+import {
+    IteratorStatePropertyCommand,
+    MappedPropertyCommand,
+    NormalNumericStateProperty,
+    NumericStateProperty,
+    NumericStatePropertyCommand,
+    StateProperty,
+} from '.';
+
+import { MapSchema } from '@colyseus/schema';
+
+export function NormalNumericStatePropertyCommand<R extends RoomName>(
+    cmdName: string,
+    setValue: (state: State<R>, value: number) => unknown,
+    getValue: (state: State<R>) => number
+) {
+    return NumericStatePropertyCommand(cmdName, setValue, getValue, [0, 1]) as NumericStatePropertyCommand<R> &
+        NormalNumericStateProperty<R>;
+}
+export function NumericStatePropertyCommand<R extends RoomName>(
+    cmdName: string,
+    setValue: (state: State<R>, value: number) => unknown,
+    getValue: (state: State<R>) => number,
+    range: [number, number] | ((state: State<R>) => [number, number])
+): NumericStatePropertyCommand<R> {
+    return { cmdName: cmdName as CommandName<R>, setValue, getValue, range };
+}
+export function NumericStateProperty<R extends RoomName>(
+    getValue: (state: State<R>) => number,
+    range: [number, number] | ((state: State<R>) => [number, number])
+): NumericStateProperty<R> {
+    return { getValue, range };
+}
+export function StringStateProperty<R extends RoomName>(
+    getValue: (state: State<R>) => string
+): StateProperty<string, 'ship'> {
+    return { getValue };
+}
+export function IteratorStatePropertyCommand<R extends RoomName>(
+    cmdName: string,
+    setValue: (state: State<R>, value: boolean) => unknown,
+    getValue: (state: State<R>) => string
+): IteratorStatePropertyCommand<R> {
+    return { cmdName: cmdName as CommandName<R>, setValue, getValue };
+}
+export function MappedPropertyCommand<R extends RoomName>(
+    cmdName: string,
+    setValue: (state: State<R>, value: [string, number]) => unknown,
+    getValue: (state: State<R>) => MapSchema<number>
+): MappedPropertyCommand<R> {
+    return { cmdName: cmdName as CommandName<R>, setValue, getValue };
+}
--- modules/model/src/logic/ship-properties.ts
+++ modules/model/src/logic/ship-properties.ts
@@ -1,64 +1,16 @@
-import { CommandName, SmartPilotMode, State, TargetedStatus } from '..';
 import {
     IteratorStatePropertyCommand,
     MappedPropertyCommand,
-    NormalNumericStateProperty,
+    NormalNumericStatePropertyCommand,
     NumericStateProperty,
     NumericStatePropertyCommand,
-    StateProperty,
-    XY,
-} from '.';
+    StringStateProperty,
+} from './property-constructors';
+import { SmartPilotMode, TargetedStatus } from '..';
 
-import { MapSchema } from '@colyseus/schema';
+import { XY } from '.';
 
-function NormalNSPC(
-    cmdName: string,
-    setValue: (state: State<'ship'>, value: number) => unknown,
-    getValue: (state: State<'ship'>) => number
-) {
-    return NSPC(cmdName, setValue, getValue, [0, 1]) as NumericStatePropertyCommand<'ship'> &
-        NormalNumericStateProperty<'ship'>;
-}
-function NSPC(
-    cmdName: string,
-    setValue: (state: State<'ship'>, value: number) => unknown,
-    getValue: (state: State<'ship'>) => number,
-    range: [number, number] | ((state: State<'ship'>) => [number, number])
-): NumericStatePropertyCommand<'ship'> {
-    return { cmdName: cmdName as CommandName<'ship'>, setValue, getValue, range };
-}
-function NSP(
-    getValue: (state: State<'ship'>) => number,
-    range: [number, number] | ((state: State<'ship'>) => [number, number])
-): NumericStateProperty<'ship'> {
-    return { getValue, range };
-}
-function SSP(getValue: (state: State<'ship'>) => string): StateProperty<string, 'ship'> {
-    return { getValue };
-}
-function TSPC(
-    cmdName: string,
-    setValue: (state: State<'ship'>, value: boolean) => unknown,
-    getValue: (state: State<'ship'>) => string
-): IteratorStatePropertyCommand<'ship'> {
-    return { cmdName: cmdName as CommandName<'ship'>, setValue, getValue };
-}
-function ISPC(
-    cmdName: string,
-    setValue: (state: State<'ship'>, value: boolean) => unknown,
-    getValue: (state: State<'ship'>) => string
-): IteratorStatePropertyCommand<'ship'> {
-    return { cmdName: cmdName as CommandName<'ship'>, setValue, getValue };
-}
-function MSPC(
-    cmdName: string,
-    setValue: (state: State<'ship'>, value: [string, number]) => unknown,
-    getValue: (state: State<'ship'>) => MapSchema<number>
-): MappedPropertyCommand<'ship'> {
-    return { cmdName: cmdName as CommandName<'ship'>, setValue, getValue };
-}
-
-export const smartPilotRotation = NSPC(
+export const smartPilotRotation = NumericStatePropertyCommand<'ship'>(
     'setSmartPilotRotation',
     (state, value) => {
         state.smartPilot.rotation = value;
@@ -66,7 +18,7 @@ export const smartPilotRotation = NSPC(
     (state) => state.smartPilot.rotation,
     [-1, 1]
 );
-export const smartPilotStrafe = NSPC(
+export const smartPilotStrafe = NumericStatePropertyCommand<'ship'>(
     'smartPilotStrafe',
     (state, value) => {
         state.smartPilot.maneuvering.y = value;
@@ -74,7 +26,7 @@ export const smartPilotStrafe = NSPC(
     (state) => state.smartPilot.maneuvering.y,
     [-1, 1]
 );
-export const smartPilotBoost = NSPC(
+export const smartPilotBoost = NumericStatePropertyCommand<'ship'>(
     'smartPilotBoost',
     (state, value) => {
         state.smartPilot.maneuvering.x = value;
@@ -82,98 +34,98 @@ export const smartPilotBoost = NSPC(
     (state) => state.smartPilot.maneuvering.x,
     [-1, 1]
 );
-export const rotation = NSP((state) => state.rotation, [-1, 1]);
-export const strafe = NSP((state) => state.strafe, [-1, 1]);
-export const boost = NSP((state) => state.boost, [-1, 1]);
-export const energy = NSP(
+export const rotation = NumericStateProperty<'ship'>((state) => state.rotation, [-1, 1]);
+export const strafe = NumericStateProperty<'ship'>((state) => state.strafe, [-1, 1]);
+export const boost = NumericStateProperty<'ship'>((state) => state.boost, [-1, 1]);
+export const energy = NumericStateProperty<'ship'>(
     (state) => state.energy,
     (state) => [0, state.maxEnergy]
 );
-export const reserveSpeed = NSP(
+export const reserveSpeed = NumericStateProperty<'ship'>(
     (state) => state.reserveSpeed,
     (state) => [0, state.maxReserveSpeed]
 );
-export const turnSpeed = NSP((state) => state.turnSpeed, [-90, 90]);
-export const angle = NSP((state) => state.angle, [0, 360]);
-export const velocityAngle = NSP((state) => XY.angleOf(state.velocity), [0, 360]);
-export const speed = NSP(
+export const turnSpeed = NumericStateProperty<'ship'>((state) => state.turnSpeed, [-90, 90]);
+export const angle = NumericStateProperty<'ship'>((state) => state.angle, [0, 360]);
+export const velocityAngle = NumericStateProperty<'ship'>((state) => XY.angleOf(state.velocity), [0, 360]);
+export const speed = NumericStateProperty<'ship'>(
     (state) => XY.lengthOf(state.velocity),
     (state) => [0, state.maxMaxSpeed]
 );
-export const chainGunCoolDown = NSP((state) => state.chainGun.cooldown, [0, 1]);
-export const chainGunSellSecondsToLive = NSP(
+export const chainGunCoolDown = NumericStateProperty<'ship'>((state) => state.chainGun.cooldown, [0, 1]);
+export const chainGunSellSecondsToLive = NumericStateProperty<'ship'>(
     (state) => state.chainGun.shellSecondsToLive,
     (state) => [state.chainGun.minShellSecondsToLive, state.chainGun.maxShellSecondsToLive]
 );
-export const useReserveSpeed = NormalNSPC(
+export const useReserveSpeed = NormalNumericStatePropertyCommand<'ship'>(
     'useReserveSpeed',
     (state, value) => {
         state.useReserveSpeed = value;
     },
     (state) => state.useReserveSpeed
 );
-export const antiDrift = NormalNSPC(
+export const antiDrift = NormalNumericStatePropertyCommand<'ship'>(
     'antiDrift',
     (state, value) => {
         state.antiDrift = value;
     },
     (state) => state.antiDrift
 );
-export const breaks = NormalNSPC(
+export const breaks = NormalNumericStatePropertyCommand<'ship'>(
     'breaks',
     (state, value) => {
         state.breaks = value;
     },
     (state) => state.breaks
 );
-export const chainGunIsFiring = TSPC(
+export const chainGunIsFiring = IteratorStatePropertyCommand<'ship'>(
     'chainGunIsFiring',
     (state, value) => {
         state.chainGun.isFiring = value;
     },
     (state) => (state.chainGun.isFiring ? 'FIRE' : 'NONE')
 );
-export const targeted = SSP((state) => TargetedStatus[state.targeted]);
-export const target = ISPC(
+export const targeted = StringStateProperty<'ship'>((state) => TargetedStatus[state.targeted]);
+export const target = IteratorStatePropertyCommand<'ship'>(
     'target',
     (state, value) => {
         state.nextTargetCommand = value;
     },
     (state) => String(state.targetId)
 );
-export const rotationMode = ISPC(
+export const rotationMode = IteratorStatePropertyCommand<'ship'>(
     'rotationMode',
     (state, value) => {
         state.rotationModeCommand = value;
     },
     (state) => SmartPilotMode[state.smartPilot.rotationMode]
 );
-export const maneuveringMode = ISPC(
+export const maneuveringMode = IteratorStatePropertyCommand<'ship'>(
     'maneuveringMode',
     (state, value) => {
         state.maneuveringModeCommand = value;
     },
     (state) => SmartPilotMode[state.smartPilot.maneuveringMode]
 );
-export const constants = MSPC(
+export const constants = MappedPropertyCommand<'ship'>(
     'constants',
     (state, [name, value]) => {
         state.constants.set(name, value);
     },
     (state) => state.constants
 );
-export const chainGunConstants = MSPC(
+export const chainGunConstants = MappedPropertyCommand<'ship'>(
     'chainGunConstants',
     (state, [name, value]) => {
         state.chainGun.constants.set(name, value);
     },
     (state) => state.chainGun.constants
 );
-export const shellSecondsToLive = NSP(
+export const shellSecondsToLive = NumericStateProperty<'ship'>(
     (state) => state.chainGun.shellSecondsToLive,
     (state) => [state.chainGun.minShellSecondsToLive, state.chainGun.maxShellSecondsToLive]
 );
-export const shellRange = NSPC(
+export const shellRange = NumericStatePropertyCommand<'ship'>(
     'shellRange',
     (state, value) => {
         state.chainGun.shellRange = value;
