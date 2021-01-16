1eaacf21910b5807de3eac9a294d13c2bdf29e21
Amir Arad <greenshade@gmail.com>
2021-01-16 14:20:55 +0200

    organise build tasks

--- scripts/pkg.js
+++ scripts/pkg.js
@@ -1,52 +1,13 @@
 /* eslint-disable no-console */
 const path = require('path');
-const fs = require('fs');
 const util = require('util');
-const ncp = util.promisify(require('ncp').ncp);
-const rimraf = util.promisify(require('rimraf'));
-const pkg = require('pkg');
-const mkdir = util.promisify(fs.mkdir);
 const exec = util.promisify(require('child_process').exec);
-const writeFile = util.promisify(fs.writeFile);
+const pkg = require('pkg');
 const rootPath = path.resolve(__dirname, '..');
 const distPath = path.join(rootPath, 'dist');
-const staticDistPath = path.join(distPath, 'static');
-const codeDistPath = path.join(distPath, 'code');
-const serverModulePath = path.join(rootPath, 'modules', 'server');
 (async () => {
     try {
-        await rimraf(distPath);
-        await mkdir(distPath);
-        await mkdir(staticDistPath);
-        await mkdir(codeDistPath);
-        await ncp(path.join(rootPath, 'static'), staticDistPath);
-        await ncp(path.join(rootPath, 'modules', 'browser', 'dist'), staticDistPath);
-        await ncp(path.join(serverModulePath, 'cjs'), codeDistPath);
-        const dependencies = require(path.join(serverModulePath, 'package.json')).dependencies;
-
-        // todo: generify
-        dependencies['@starwards/model'] = path.join(rootPath, 'modules', 'model');
-
-        await writeFile(
-            path.join(distPath, 'package.json'),
-            JSON.stringify(
-                {
-                    name: 'starwards',
-                    bin: 'code/prod.js',
-                    scripts: {
-                        start: 'node code/prod.js',
-                    },
-                    pkg: {
-                        assets: 'static/**/*',
-                        targets: ['node10-win-x64', 'node10-linux-x64', 'node10-osx-x64'],
-                    },
-                    dependencies,
-                },
-                null,
-                2
-            )
-        );
-        const { stdout, stderr } = await exec('yarn', { cwd: distPath });
+        const { stdout, stderr } = await exec('npm install --legacy-peer-deps', { cwd: distPath });
         console.error(stderr);
         console.log(stdout);
         await pkg.exec([distPath, '--out-path', distPath]);
---
+++ scripts/post-build.js
@@ -0,0 +1,63 @@
+/* eslint-disable no-console */
+const path = require('path');
+const fs = require('fs');
+const util = require('util');
+const exec = util.promisify(require('child_process').exec);
+const ncp = util.promisify(require('ncp').ncp);
+const rimraf = util.promisify(require('rimraf'));
+const mkdir = util.promisify(fs.mkdir);
+const writeFile = util.promisify(fs.writeFile);
+const rootPath = path.resolve(__dirname, '..');
+const distPath = path.join(rootPath, 'dist');
+const staticDistPath = path.join(distPath, 'static');
+const serverDistPath = path.join(distPath, 'server');
+const serverModulePath = path.join(rootPath, 'modules', 'server');
+const modelDistPath = path.join(distPath, 'model');
+const modelModulePath = path.join(rootPath, 'modules', 'model');
+(async () => {
+    try {
+        await rimraf(distPath);
+        await mkdir(distPath);
+        await mkdir(staticDistPath);
+        await mkdir(serverDistPath);
+        await mkdir(modelDistPath);
+        await ncp(path.join(rootPath, 'static'), staticDistPath);
+        await ncp(path.join(rootPath, 'modules', 'browser', 'dist'), staticDistPath);
+        await ncp(path.join(serverModulePath, 'cjs'), serverDistPath);
+        await ncp(path.join(rootPath, '.vscode', '.deployment'), path.join(distPath, '.deployment'));
+        const { stdout, stderr } = await exec('npm pack ' + modelModulePath, { cwd: distPath });
+        console.error(stderr);
+        console.log(stdout);
+        const modulePackageFileName = stdout.trim().split('\n').pop().trim(); // last line is the file name
+        const serverDependencies = require(path.join(serverModulePath, 'package.json')).dependencies;
+        const modelDependencies = require(path.join(serverModulePath, 'package.json')).dependencies;
+
+        const dependencies = {
+            ...modelDependencies,
+            ...serverDependencies,
+            '@starwards/model': 'file:./' + modulePackageFileName,
+        };
+
+        await writeFile(
+            path.join(distPath, 'package.json'),
+            JSON.stringify(
+                {
+                    name: 'starwards',
+                    bin: 'server/prod.js',
+                    scripts: {
+                        start: 'node server/prod.js',
+                    },
+                    pkg: {
+                        assets: 'static/**/*',
+                        targets: ['node10-win-x64', 'node10-linux-x64', 'node10-osx-x64'],
+                    },
+                    dependencies,
+                },
+                null,
+                2
+            )
+        );
+    } catch (e) {
+        console.error(e);
+    }
+})();
