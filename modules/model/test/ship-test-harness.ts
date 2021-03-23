5063f0855eda7b93b21c8218cb4ed2b54f0017d5
Amir Arad <greenshade@gmail.com>
2021-03-23 23:23:30 +0200

    dereference afterBurner command rename properties

--- modules/model/test/ship-test-harness.ts
+++ modules/model/test/ship-test-harness.ts
@@ -103,7 +103,7 @@ export class ShipTestHarness {
         this.graphBuilder?.newPoint(0);
         for (let i = 0; i < iterations; i++) {
             const p = this.graphBuilder?.newPoint(iterationTimeInSeconds);
-            this.shipState.afterBurner = this.shipState.maxAfterBurner;
+            this.shipState.afterBurnerFuel = this.shipState.maxAfterBurner;
             this.shipState.energy = this.shipState.maxEnergy;
             body && body(iterationTimeInSeconds, p);
             this.shipMgr.update(iterationTimeInSeconds);
