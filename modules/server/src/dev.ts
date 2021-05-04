54aca1412d04a58bfda3ff5d54f6e3fcf4b8c3c3
Yuval Moran <28506179+gmyuval@users.noreply.github.com>
2021-05-04 20:54:59 +0300

    Mqtt ship state (#164)
    * added mqtt-client and sihp-state-messenger
    
    * added default values for mqtt host/port
    
    * Co-authored-by: Amir Arad <amir-arad@users.noreply.github.com>
    
    * changed update structure
    
    * changed package.json to run tests per workspace
    
    * circleci config.yml change

--- modules/server/src/dev.ts
+++ modules/server/src/dev.ts
@@ -2,4 +2,9 @@ import * as path from 'path';
 
 import { server } from './server';
 
-void server(Number(process.env.PORT || 8080), path.resolve(__dirname, '..', '..', '..', 'static'));
+void server(
+    Number(process.env.PORT || 8080),
+    path.resolve(__dirname, '..', '..', '..', 'static'),
+    String(process.env.MQTT || 'http://localhost'),
+    Number(process.env.MQTT_PORT || 1883)
+);
--- modules/server/src/prod.ts
+++ modules/server/src/prod.ts
@@ -3,7 +3,9 @@ import * as path from 'path';
 import { server } from './server';
 
 const port = Number(process.env.PORT || 80);
+const mqttHostame = String(process.env.MQTT || 'http://localhost');
+const mqttPort = Number(process.env.MQTT_PORT || 1883);
 // this path has to match the setup in scripts/pkg.js
-void server(port, path.join(__dirname, '..', 'static'));
+void server(port, path.join(__dirname, '..', 'static'), mqttHostame, mqttPort);
 
 // for local debugging run server(80, path.join(__dirname, '..', '..', 'browser', 'dist'));
