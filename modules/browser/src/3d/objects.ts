dbbb31b5c1648fd85052bd59dc6d37c7c2a920ce
Amir Arad <greenshade@gmail.com>
2021-05-28 19:31:38 +0300

    simplify 3d objects logic

--- modules/browser/src/3d/objects.ts
+++ modules/browser/src/3d/objects.ts
@@ -1,15 +1,13 @@
 import { SpaceObject, State, degToRad } from '@starwards/model';
 
 import { AbstractMesh } from '@babylonjs/core';
-import EventEmitter from 'eventemitter3';
 import { Meshes } from './meshes';
 
 export class Objects3D {
-    private graphics: { [id: string]: ObjectGraphics } = {};
-    private toReDraw = new Set<ObjectGraphics>();
-    constructor(private spaceState: State<'space'>, private meshes: Meshes, _shipId: string) {
+    private graphics = new Map<string, ObjectGraphics>();
+    constructor(spaceState: State<'space'>, private meshes: Meshes, _shipId: string) {
         spaceState.events.on('add', (spaceObject: SpaceObject) => this.onNewSpaceObject(spaceObject));
-        spaceState.events.on('remove', (spaceObject: SpaceObject) => this.cleanupSpaceObject(spaceObject.id));
+        spaceState.events.on('remove', (spaceObject: SpaceObject) => this.graphics.get(spaceObject.id)?.destroy());
 
         for (const spaceObject of spaceState) {
             this.onNewSpaceObject(spaceObject);
@@ -17,18 +15,9 @@ export class Objects3D {
     }
 
     public onRender = () => {
-        for (const objGraphics of this.toReDraw) {
-            if (objGraphics.isDestroyed()) {
-                this.cleanupSpaceObject(objGraphics.spaceObject.id);
-            } else {
-                if (!this.spaceState.get(objGraphics.spaceObject.id)) {
-                    // eslint-disable-next-line no-console
-                    console.warn(`found zombie object! ${objGraphics.spaceObject.id}`);
-                }
-                objGraphics.redraw();
-            }
+        for (const objGraphics of this.graphics.values()) {
+            objGraphics.redraw();
         }
-        // this.toReDraw.clear();
     };
 
     private onNewSpaceObject(spaceObject: SpaceObject) {
@@ -53,40 +42,8 @@ export class Objects3D {
                 console.log('error adding 3d obj', e);
                 return;
             }
-            //  && spaceObject.id !== this.shipId
-            const objGraphics = new ObjectGraphics(spaceObject, mesh);
-            this.toReDraw.add(objGraphics);
-
-            // objGraphics.listen(this.spaceState.events, spaceObject.id, (_field: string) => {
-            //     this.toReDraw.add(objGraphics);
-            // });
-            this.graphics[spaceObject.id] = objGraphics;
-            // this.stage.addChild(objGraphics.stage);
-            // objGraphics.listen(this.parent.events as EventEmitter, 'screenChanged', () => {
-            //     objGraphics.markChanged('screen');
-            //     this.toReDraw.add(objGraphics);
-            // });
-            // objGraphics.listen(this.parent.events as EventEmitter, 'angleChanged', () => {
-            //     objGraphics.markChanged('parentAngle');
-            //     this.toReDraw.add(objGraphics);
-            // });
-            // objGraphics.listen(this.room.state.events, spaceObject.id, (field: string) => {
-            //     objGraphics.markChanged(field);
-            //     this.toReDraw.add(objGraphics);
-            // });
-            // objGraphics.listen(this.selectedItems.events, spaceObject.id, () => {
-            //     objGraphics.markChanged('selected');
-            //     this.toReDraw.add(objGraphics);
-            // });
-        }
-    }
-
-    private cleanupSpaceObject(id: string) {
-        const objGraphics = this.graphics[id];
-        if (objGraphics) {
-            delete this.graphics[id];
-            objGraphics.destroy();
-            this.toReDraw.delete(objGraphics);
+            const objGraphics = new ObjectGraphics(spaceObject, mesh, () => this.graphics.delete(spaceObject.id));
+            this.graphics.set(spaceObject.id, objGraphics);
         }
     }
 }
@@ -95,40 +52,20 @@ export class Objects3D {
  */
 // eslint-disable-next-line: max-classes-per-file
 class ObjectGraphics {
-    private disposables: Array<() => void> = [];
-    private destroyed = false;
-    // private mesh: AbstractMesh | null = null;
+    constructor(public spaceObject: SpaceObject, private mesh: AbstractMesh, private onDestroyed: () => unknown) {}
 
-    constructor(public spaceObject: SpaceObject, private mesh: AbstractMesh) {}
-
-    isDestroyed() {
-        return this.spaceObject.destroyed || this.destroyed;
-    }
-
-    // eslint-disable-next-line @typescript-eslint/no-empty-function
     redraw() {
-        // if (this.mesh) {
-        this.mesh.position.x = this.spaceObject.position.x;
-        this.mesh.position.z = -this.spaceObject.position.y;
-        this.mesh.rotation.y = degToRad * this.spaceObject.angle + Math.PI / 2;
-        // }
-    }
-
-    // eslint-disable-next-line @typescript-eslint/no-explicit-any
-    listen(events: EventEmitter, event: string, listener: (...args: any[]) => any) {
-        if (!this.isDestroyed()) {
-            events.on(event, listener);
-            this.disposables.push(() => events.off(event, listener));
+        if (this.spaceObject.destroyed) {
+            this.destroy();
+        } else {
+            this.mesh.position.x = this.spaceObject.position.x;
+            this.mesh.position.z = -this.spaceObject.position.y;
+            this.mesh.rotation.y = degToRad * this.spaceObject.angle + Math.PI / 2;
         }
     }
 
     destroy() {
-        if (!this.destroyed) {
-            this.mesh && this.mesh.dispose();
-            for (const d of this.disposables) {
-                d();
-            }
-            this.destroyed = true;
-        }
+        this.onDestroyed();
+        this.mesh && this.mesh.dispose();
     }
 }
