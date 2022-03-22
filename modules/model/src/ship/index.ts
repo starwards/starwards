99cd8fb548737ec1406d7dbc2c9401fdba587635
Amir Arad <greenshade@gmail.com>
2022-03-22 23:31:50 +0200

    fix build

--- modules/model/src/ship/index.ts
+++ modules/model/src/ship/index.ts
@@ -3,7 +3,6 @@ export * from './ship-state';
 export * from './chain-gun';
 export * from './ship-manager';
 export * from './ship-direction';
-export * from './ship-system';
 export * from './ship-areas';
 
 import * as _shipProperties from './ship-properties';
