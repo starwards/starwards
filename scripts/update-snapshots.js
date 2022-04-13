a1ec3e605c4b5e3ca67b70806b9bc468ed7e02f1
Amir Arad <greenshade@gmail.com>
2022-04-13 15:52:30 +0300

    scripts to update snapshots for CI

---
+++ scripts/update-snapshots.js
@@ -0,0 +1,32 @@
+/* eslint-disable no-console */
+const path = require('path');
+const util = require('util');
+const exec = require('child_process').exec;
+const fs = require('fs');
+
+const rootPath = path.resolve(__dirname, '..');
+function run(cmd) {
+    return new Promise((res, rej) => {
+        const dockerProc = exec(cmd, { cwd: rootPath });
+        dockerProc.stdout.pipe(process.stdout);
+        dockerProc.stderr.pipe(process.stderr);
+        dockerProc.on('exit', (code) => (code ? rej(new Error(code)) : res()));
+    });
+}
+
+(async () => {
+    try {
+        console.log('reinstalling for linux...');
+        await run(`docker run --rm -v ${rootPath}:/work/ -w /work/ mcr.microsoft.com/playwright:focal npm ci`);
+        console.log('generating snapshots...');
+        await run(
+            `docker run --rm -v ${rootPath}:/work/ -w /work/ mcr.microsoft.com/playwright:focal npm run test:e2e -- --update-snapshots`
+        );
+        console.log('done!');
+    } catch (e) {
+        console.error(e);
+    } finally {
+        console.log('reinstalling...');
+        await run(`npm ci`);
+    }
+})();
