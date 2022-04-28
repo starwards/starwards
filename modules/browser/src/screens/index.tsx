d89679709020842c86d835422582a9917c08e806
Amir Arad <greenshade@gmail.com>
2022-04-28 12:57:56 +0300

    upgrade react-dom and types

--- modules/browser/src/screens/index.tsx
+++ modules/browser/src/screens/index.tsx
@@ -1,7 +1,11 @@
 import { Driver } from '../driver';
 import { Lobby } from '../widgets/lobby';
 import React from 'react';
-import ReactDOM from 'react-dom';
+import { createRoot } from 'react-dom/client';
 
 const driver = new Driver();
-ReactDOM.render(<Lobby driver={driver} />, document.querySelector('#wrapper'));
+const container = document.getElementById('wrapper');
+if (!container) {
+    throw new Error('cant find root wrapper for react');
+}
+createRoot(container).render(<Lobby driver={driver} />);
