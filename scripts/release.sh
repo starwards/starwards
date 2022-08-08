091e647a858cf3b8b23e856db13c506132ddeb8c
Amir Arad <greenshade@gmail.com>
2022-08-08 15:38:58 +0300

    fix release script

--- scripts/release.sh
+++ scripts/release.sh
@@ -9,19 +9,20 @@ fi
 
 cd ./modules/core
 
-NEW_VERSION=$(npm version $1)
+npm version --no-fund --no-audit $1
+
+NEW_VERSION=v$(npm pkg get 'version' --ws=false | tr -d '"')
 CURRENT_BRANCH=$(git rev-parse --abbrev-ref HEAD)
 
-git add package.json package-lock.json
-git commit -m 'Bump version'
-git tag $NEW_VERSION
+git add ./package.json ../../package-lock.json
+git commit -s -m "$NEW_VERSION"
+git tag -s "$NEW_VERSION"
 echo "Bumped version to $NEW_VERSION"
 
 if [ $CURRENT_BRANCH = "master" ]
 then 
     PUSH = "y"
 else
-    # Prompt for pushing
     read -p "Current branch is $CURRENT_BRANCH, push HEAD and tags to $NEW_VERSION? y/n " PUSH
 fi
 if [ $PUSH = "y" ]
