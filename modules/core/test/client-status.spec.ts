d36386ffdf9e00d14e0a101e2cd1c2d534badad0
Amir Arad <greenshade@gmail.com>
2022-09-05 21:46:19 +0300

    reproduce disconnection status bug with test

--- modules/core/test/client-status.spec.ts
+++ modules/core/test/client-status.spec.ts
@@ -7,10 +7,13 @@ import { test_map_1 } from '@starwards/server/src/maps';
 const waitForStatus = (actual: () => Promise<StatusInfo>, expected: Partial<StatusInfo>) =>
     waitFor(async () => expect(await actual()).toEqual(expected), 3_000);
 
+const connectedNoActiveGame = Object.freeze({ status: Status.CONNECTED, text: 'no active game' });
+const shipFound = Object.freeze({ status: Status.SHIP_FOUND, text: '' });
+
 describe('client status', () => {
     describe('with no server', () => {
         const clientDriver = makeClient(() => `http://localhost:1/`);
-        it('no connection', async () => {
+        it('getStatus() returns no connection', async () => {
             const status = new ClientStatus(clientDriver.driver);
             await waitForStatus(
                 status.getStatus,
@@ -24,38 +27,58 @@ describe('client status', () => {
     describe('with server', () => {
         const gameDriver = makeDriver();
         const clientDriver = makeClient(gameDriver.url);
-        it('connected', async () => {
+        it('getStatus() returns connected', async () => {
             const status = new ClientStatus(clientDriver.driver);
-            await waitForStatus(status.getStatus, {
-                status: Status.CONNECTED,
-                text: 'no active game',
-            });
+            await waitForStatus(status.getStatus, connectedNoActiveGame);
         });
         describe('and a active game', () => {
             beforeEach(async () => {
                 await gameDriver.gameManager.startGame(test_map_1);
             });
-            it('game running', async () => {
+            it('getStatus() returns game running', async () => {
                 const status = new ClientStatus(clientDriver.driver);
                 await waitForStatus(status.getStatus, {
                     status: Status.GAME_RUNNING,
                     text: '',
                 });
             });
-            it('ship not found', async () => {
+            it('getStatus() returns ship not found', async () => {
                 const status = new ClientStatus(clientDriver.driver, 'wrong_ship_id');
                 await waitForStatus(status.getStatus, {
                     status: Status.GAME_RUNNING,
                     text: 'ship not found',
                 });
             });
-            it('ship not found', async () => {
+            it('getStatus() returns ship found', async () => {
                 const status = new ClientStatus(clientDriver.driver, test_map_1.testShipId);
-                await waitForStatus(status.getStatus, {
-                    status: Status.SHIP_FOUND,
-                    text: '',
-                });
+                await waitForStatus(status.getStatus, shipFound);
             });
+        });
+        it('onStatusChange() emits current status', async () => {
+            await waitFor(() => expect(clientDriver.driver.isConnected).toBeTruthy(), 3_000);
+            const statuses: StatusInfo[] = [];
+            new ClientStatus(clientDriver.driver).onStatusChange((s) => statuses.push(s));
+            await waitFor(() => expect(statuses).toEqual([connectedNoActiveGame]), 100);
+        });
+        it('onStatusChange() tracks changes', async () => {
+            const statuses: StatusInfo[] = [];
+            new ClientStatus(clientDriver.driver, test_map_1.testShipId).onStatusChange((s) => statuses.push(s));
+            await waitFor(() => expect(statuses.pop()).toEqual(connectedNoActiveGame), 3_000);
+            await gameDriver.gameManager.startGame(test_map_1);
+            await waitFor(() => expect(statuses.pop()).toEqual(shipFound), 3_000);
+        });
+        it.only('onStatusChange() detects disconnection', async () => {
+            const statuses: StatusInfo[] = [];
+            new ClientStatus(clientDriver.driver, test_map_1.testShipId).onStatusChange((s) => statuses.push(s));
+            await gameDriver.gameManager.startGame(test_map_1);
+            await waitFor(() => expect(statuses.pop()).toEqual(shipFound), 3_000);
+            gameDriver.httpServer.close();
+            gameDriver.httpServer.closeAllConnections();
+            await waitFor(
+                () =>
+                    expect(statuses).toEqual([expect.objectContaining({ status: Status.DISCONNECTED }) as StatusInfo]),
+                3_000
+            );
         });
     });
 });
