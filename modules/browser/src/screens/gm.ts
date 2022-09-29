94e70d2a436f27007d3fd6746dca522c90368702
Amir Arad <greenshade@gmail.com>
2022-09-29 16:00:46 +0300

    gm screen restarts when no game

--- modules/browser/src/screens/gm.ts
+++ modules/browser/src/screens/gm.ts
@@ -1,9 +1,9 @@
 // import * as PIXI from 'pixi.js';
 
+import { ClientStatus, Driver, Status } from '@starwards/core';
 import { Dashboard, getGoldenLayoutItemConfig } from '../widgets/dashboard';
 
 import $ from 'jquery';
-import { Driver } from '@starwards/core';
 import { GmWidgets } from '../widgets/gm';
 import { InputManager } from '../input/input-manager';
 import { armorWidget } from '../widgets/armor';
@@ -21,51 +21,60 @@ import { targetRadarWidget } from '../widgets/target-radar';
 // https://chrome.google.com/webstore/detail/pixijs-devtools/aamddddknhcagpehecnhphigffljadon
 // window.PIXI = PIXI;
 const driver = new Driver(window.location).connect();
-const gmWidgets = new GmWidgets(driver);
-const dashboard = new Dashboard(
-    {
-        content: [
+const statusTracker = new ClientStatus(driver);
+
+void driver.waitForGame().then(
+    async () => {
+        statusTracker.onStatusChange(({ status }) => {
+            if (status !== Status.GAME_RUNNING) location.reload();
+        });
+        const gmWidgets = new GmWidgets(driver);
+        const dashboard = new Dashboard(
             {
                 content: [
-                    { ...getGoldenLayoutItemConfig(gmWidgets.radar), width: 80 },
-                    { ...getGoldenLayoutItemConfig(gmWidgets.tweak), width: 20 },
+                    {
+                        content: [
+                            { ...getGoldenLayoutItemConfig(gmWidgets.radar), width: 80 },
+                            { ...getGoldenLayoutItemConfig(gmWidgets.tweak), width: 20 },
+                        ],
+                        isClosable: true,
+                        title: '',
+                        type: 'row',
+                    },
                 ],
-                isClosable: true,
-                title: '',
-                type: 'row',
             },
-        ],
-    },
-    $('#layoutContainer'),
-    $('#menuContainer')
-);
+            $('#layoutContainer'),
+            $('#menuContainer')
+        );
 
-dashboard.registerWidget(gmWidgets.radar);
-dashboard.registerWidget(gmWidgets.tweak);
+        dashboard.registerWidget(gmWidgets.radar);
+        dashboard.registerWidget(gmWidgets.tweak);
 
-dashboard.setup();
-// constantly scan for new ships and add widgets for them
-void (async () => {
-    const spaceDriver = await driver.getSpaceDriver();
-    const spaceActions = spaceDriver.selectionActions(() => gmWidgets.selectionContainer.selectedItemsIds);
-
-    const input = new InputManager();
-    input.addStepsAction(spaceActions.rotate, gmInputConfig.rotate);
-    input.addKeyAction(spaceActions.toggleFreeze, gmInputConfig.toggleFreeze);
-
-    input.init();
-
-    for await (const shipId of driver.getUniqueShipIds()) {
-        const shipDriver = await driver.getShipDriver(shipId);
-        dashboard.registerWidget(radarWidget(spaceDriver, shipDriver), {}, shipId + ' radar');
-        dashboard.registerWidget(tacticalRadarWidget(spaceDriver, shipDriver), {}, shipId + ' tactical radar');
-        dashboard.registerWidget(pilotWidget(shipDriver), {}, shipId + ' helm');
-        dashboard.registerWidget(gunWidget(shipDriver), {}, shipId + ' gun');
-        dashboard.registerWidget(shipConstantsWidget(shipDriver), { shipDriver }, shipId + ' constants');
-        dashboard.registerWidget(targetRadarWidget(spaceDriver, shipDriver), {}, shipId + ' target radar');
-        dashboard.registerWidget(monitorWidget(shipDriver), {}, shipId + ' monitor');
-        dashboard.registerWidget(damageReportWidget(shipDriver), {}, shipId + ' damage report');
-        dashboard.registerWidget(armorWidget(shipDriver), {}, shipId + ' armor');
         dashboard.setup();
-    }
-})();
+        // constantly scan for new ships and add widgets for them
+        const spaceDriver = await driver.getSpaceDriver();
+        const spaceActions = spaceDriver.selectionActions(() => gmWidgets.selectionContainer.selectedItemsIds);
+
+        const input = new InputManager();
+        input.addStepsAction(spaceActions.rotate, gmInputConfig.rotate);
+        input.addKeyAction(spaceActions.toggleFreeze, gmInputConfig.toggleFreeze);
+
+        input.init();
+
+        for await (const shipId of driver.getUniqueShipIds()) {
+            const shipDriver = await driver.getShipDriver(shipId);
+            dashboard.registerWidget(radarWidget(spaceDriver, shipDriver), {}, shipId + ' radar');
+            dashboard.registerWidget(tacticalRadarWidget(spaceDriver, shipDriver), {}, shipId + ' tactical radar');
+            dashboard.registerWidget(pilotWidget(shipDriver), {}, shipId + ' helm');
+            dashboard.registerWidget(gunWidget(shipDriver), {}, shipId + ' gun');
+            dashboard.registerWidget(shipConstantsWidget(shipDriver), { shipDriver }, shipId + ' constants');
+            dashboard.registerWidget(targetRadarWidget(spaceDriver, shipDriver), {}, shipId + ' target radar');
+            dashboard.registerWidget(monitorWidget(shipDriver), {}, shipId + ' monitor');
+            dashboard.registerWidget(damageReportWidget(shipDriver), {}, shipId + ' damage report');
+            dashboard.registerWidget(armorWidget(shipDriver), {}, shipId + ' armor');
+            dashboard.setup();
+        }
+    },
+    // eslint-disable-next-line no-console
+    (e) => console.error(e)
+);
