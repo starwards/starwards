f51ce2801345f2b8bf0667eb26262168f4a4197c
Amir Arad <greenshade@gmail.com>
2022-09-29 15:49:02 +0300

    client ship screens now reconnect automatically

--- modules/browser/src/screens/main-screen.ts
+++ modules/browser/src/screens/main-screen.ts
@@ -1,4 +1,5 @@
-import { Driver } from '@starwards/core';
+import { ClientStatus, Driver, Status } from '@starwards/core';
+
 // https://github.com/RaananW/babylonjs-webpack-es6/tree/master/src
 import { Engine } from '@babylonjs/core';
 import { Objects3D } from '../3d/objects';
@@ -9,53 +10,58 @@ import { wireSinglePilotInput } from '../input/wiring';
 const driver = new Driver(window.location).connect();
 
 const NUM_OF_FRAMES_FOR_LOADED = 1;
-const babylonInit = async (): Promise<void> => {
-    // todo extract to configurable widget
 
-    const urlParams = new URLSearchParams(window.location.search);
-    const shipId = urlParams.get('ship');
-    const debug = urlParams.has('debug');
-    if (shipId) {
-        const shipDriver = await driver.getShipDriver(shipId);
-        const spaceDriver = await driver.getSpaceDriver();
-        // Get the canvas element
-        const canvas = document.getElementById('renderCanvas') as HTMLCanvasElement;
-        canvas.setAttribute('data-loaded', `false`);
-        // Generate the BABYLON 3D engine
-        const engine = new Engine(canvas, true);
+const urlParams = new URLSearchParams(window.location.search);
+const shipId = urlParams.get('ship');
+const debug = urlParams.has('debug');
+if (shipId) {
+    const statusTracker = new ClientStatus(driver, shipId);
+    void driver.waitForShip(shipId).then(
+        async () => {
+            const shipDriver = await driver.getShipDriver(shipId);
+            statusTracker.onStatusChange(({ status }) => {
+                if (status !== Status.SHIP_FOUND) location.reload();
+            });
+            const spaceDriver = await driver.getSpaceDriver();
+            // Get the canvas element
+            const canvas = document.getElementById('renderCanvas') as HTMLCanvasElement;
+            canvas.setAttribute('data-loaded', `false`);
+            // Generate the BABYLON 3D engine
+            const engine = new Engine(canvas, true);
 
-        // Create the scene
-        const scene = placeSceneEnv(engine);
-        const meshes = await loadMeshes(scene);
-        const objects = new Objects3D(spaceDriver, meshes, shipId);
-        // Register a render loop to repeatedly render the scene
-        let frameNum = 0;
-        engine.runRenderLoop(function () {
-            objects.onRender();
-            scene.render();
-            if (scene.isReady() && frameNum <= NUM_OF_FRAMES_FOR_LOADED) {
-                frameNum++;
+            // Create the scene
+            const scene = placeSceneEnv(engine);
+            const meshes = await loadMeshes(scene);
+            const objects = new Objects3D(spaceDriver, meshes, shipId);
+            // Register a render loop to repeatedly render the scene
+            let frameNum = 0;
+            engine.runRenderLoop(function () {
+                objects.onRender();
+                scene.render();
+                if (scene.isReady() && frameNum <= NUM_OF_FRAMES_FOR_LOADED) {
+                    frameNum++;
+                }
+                if (frameNum === NUM_OF_FRAMES_FOR_LOADED) {
+                    canvas.setAttribute('data-loaded', `true`);
+                }
+            });
+
+            // Watch for browser/canvas resize events
+            window.addEventListener('resize', function () {
+                engine.resize();
+            });
+            // scene started rendering, everything is initialized
+            if (debug) {
+                wireSinglePilotInput(shipDriver);
+                await scene.debugLayer.show();
+                // eslint-disable-next-line no-console
+                console.log('loaded!');
             }
-            if (frameNum === NUM_OF_FRAMES_FOR_LOADED) {
-                canvas.setAttribute('data-loaded', `true`);
-            }
-        });
-
-        // Watch for browser/canvas resize events
-        window.addEventListener('resize', function () {
-            engine.resize();
-        });
-        // scene started rendering, everything is initialized
-        if (debug) {
-            wireSinglePilotInput(shipDriver);
-            await scene.debugLayer.show();
-            // eslint-disable-next-line no-console
-            console.log('loaded!');
-        }
-    } else {
+        },
         // eslint-disable-next-line no-console
-        console.error('missing "ship" url query param');
-    }
-};
-
-void babylonInit();
+        (e) => console.error(e)
+    );
+} else {
+    // eslint-disable-next-line no-console
+    console.error('missing "ship" url query param');
+}
