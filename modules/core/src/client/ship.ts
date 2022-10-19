b7c899fbd0bebaf88362888266a385c12d6527cb
Amir Arad <greenshade@gmail.com>
2022-10-19 23:31:00 +0300

    refactor

--- modules/core/src/client/ship.ts
+++ modules/core/src/client/ship.ts
@@ -1,35 +1,11 @@
 import { GameRoom, isJsonPointer } from '..';
 import { Primitive, isPrimitive } from 'colyseus-events';
-import { RoomEventEmitter, makeEventsEmitter } from './events';
 import { readNumberProp, readProp, readWriteNumberProp, readWriteProp, writeProp } from '../api/properties';
 
+import { makeEventsEmitter } from './events';
 import { waitForEvents } from '../async-utils';
 
-export type ShipDriver = ReturnType<typeof newShipDriverObj>;
-
-function newShipDriverObj(shipRoom: GameRoom<'ship'>, events: RoomEventEmitter) {
-    return {
-        events,
-        id: shipRoom.state.id,
-        get state() {
-            return shipRoom.state;
-        },
-        setPrimitiveState: (pointerStr: string, value: Primitive) => {
-            if (!isJsonPointer(pointerStr)) {
-                throw new Error(`not a legal Json pointer: ${JSON.stringify(pointerStr)}`);
-            }
-            if (!isPrimitive(value)) {
-                throw new Error(`not a legal value: ${JSON.stringify(value)}`);
-            }
-            shipRoom.send(pointerStr, { value });
-        },
-        writeProp: <T>(pointerStr: string) => writeProp<T>(shipRoom, pointerStr),
-        readProp: <T>(pointerStr: string) => readProp<T>(shipRoom, events, pointerStr),
-        readWriteProp: <T>(pointerStr: string) => readWriteProp<T>(shipRoom, events, pointerStr),
-        readWriteNumberProp: readWriteNumberProp.bind(null, shipRoom, events),
-        readNumberProp: readNumberProp.bind(null, shipRoom, events),
-    };
-}
+export type ShipDriver = Awaited<ReturnType<typeof ShipDriver>>;
 
 export async function ShipDriver(shipRoom: GameRoom<'ship'>) {
     const events = makeEventsEmitter(shipRoom.state);
@@ -51,6 +27,25 @@ export async function ShipDriver(shipRoom: GameRoom<'ship'>) {
         pendingEvents.push('/armor');
     }
     await waitForEvents(events, pendingEvents);
-    const driver = newShipDriverObj(shipRoom, events);
-    return driver;
+    return {
+        events,
+        id: shipRoom.state.id,
+        get state() {
+            return shipRoom.state;
+        },
+        setPrimitiveState: (pointerStr: string, value: Primitive) => {
+            if (!isJsonPointer(pointerStr)) {
+                throw new Error(`not a legal Json pointer: ${JSON.stringify(pointerStr)}`);
+            }
+            if (!isPrimitive(value)) {
+                throw new Error(`not a legal value: ${JSON.stringify(value)}`);
+            }
+            shipRoom.send(pointerStr, { value });
+        },
+        writeProp: <T>(pointerStr: string) => writeProp<T>(shipRoom, pointerStr),
+        readProp: <T>(pointerStr: string) => readProp<T>(shipRoom, events, pointerStr),
+        readWriteProp: <T>(pointerStr: string) => readWriteProp<T>(shipRoom, events, pointerStr),
+        readWriteNumberProp: readWriteNumberProp.bind(null, shipRoom, events),
+        readNumberProp: readNumberProp.bind(null, shipRoom, events),
+    };
 }
