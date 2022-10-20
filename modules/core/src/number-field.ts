bfd1fc53f506b0cda46cd975e9f38e52fc98e80f
Amir Arad <greenshade@gmail.com>
2022-10-20 19:44:31 +0300

    fix reference loop bug

--- modules/core/src/number-field.ts
+++ modules/core/src/number-field.ts
@@ -1,7 +1,5 @@
 import { Schema, type } from '@colyseus/schema';
 
-import { limitPercisionHard } from './logic';
-
 export const number2Digits = ((target: typeof Schema, field: string) => {
     type('float32')(target, field);
 
@@ -11,6 +9,6 @@ export const number2Digits = ((target: typeof Schema, field: string) => {
     // eslint-disable-next-line @typescript-eslint/unbound-method
     const oldSetter = definition.descriptors[field].set;
     definition.descriptors[field].set = function (this: Schema, value: number) {
-        oldSetter?.call(this, limitPercisionHard(value));
+        oldSetter?.call(this, Math.round(value * 1e2) / 1e2);
     };
 }) as PropertyDecorator;
