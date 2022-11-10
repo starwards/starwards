c1572e8a566a80e77a999b8a874aae1df9eb0e8b
Amir Arad <greenshade@gmail.com>
2022-11-10 21:40:09 +0200

    use tweakpane for system status

---
+++ modules/browser/src/widgets/system-status.ts
@@ -0,0 +1,50 @@
+import { Destructors, ShipDriver } from '@starwards/core';
+import { abstractOnChange, readProp } from '../property-wrappers';
+
+import { Container } from 'golden-layout';
+import { DashboardWidget } from './dashboard';
+import { Pane } from 'tweakpane';
+import { addTextBlade } from '../panel';
+import { defectReadProp } from '../react/hooks';
+
+export function systemsStatusWidget(shipDriver: ShipDriver): DashboardWidget {
+    class SystemsStatus {
+        private pane: Pane;
+        private panelCleanup = new Destructors();
+        constructor(container: Container, _: unknown) {
+            this.pane = new Pane({ container: container.getElement().get(0) });
+            this.panelCleanup.add(() => {
+                this.pane.dispose();
+            });
+            container.on('destroy', this.panelCleanup.destroy);
+
+            for (const system of shipDriver.systems) {
+                const brokenProp = readProp(shipDriver, `${system.pointer}/broken`);
+                const defectibleProps = [brokenProp, ...system.defectibles.map(defectReadProp(shipDriver))];
+                const getText = () => system.getStatus();
+                const prop = {
+                    onChange: (cb: () => unknown) => abstractOnChange(defectibleProps, getText, cb),
+                    getValue: getText,
+                };
+                const guiController = addTextBlade(
+                    this.pane,
+                    prop,
+                    { label: system.state.name },
+                    this.panelCleanup.add
+                );
+                // This allows overriding tweakpane theme for this folder
+                guiController.element.classList.add('tp-rotv');
+                const applyThemeByStatus = () => (guiController.element.dataset.status = system.getStatus());
+                this.panelCleanup.add(abstractOnChange(defectibleProps, system.getStatus, applyThemeByStatus));
+                applyThemeByStatus();
+            }
+        }
+    }
+
+    return {
+        name: 'systems status',
+        type: 'component',
+        component: SystemsStatus,
+        defaultProps: {},
+    };
+}
