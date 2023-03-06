fbc2ffd8abe03001af1e7fee7e593404ce4cb98c
Amir Arad <greenshade@gmail.com>
2023-03-07 00:16:35 +0200

    fix system status heat display

--- modules/browser/src/widgets/system-status.ts
+++ modules/browser/src/widgets/system-status.ts
@@ -49,27 +49,25 @@ export function drawSystemsStatus(container: WidgetContainer, shipDriver: ShipDr
 
         const brokenProp = readProp(shipDriver, `${system.pointer}/broken`);
         const defectibleProps = [brokenProp, ...system.defectibles.map(defectReadProp(shipDriver))];
-        const getText = () => system.getStatus();
         const statusProp = {
-            onChange: (cb: () => unknown) => abstractOnChange(defectibleProps, getText, cb),
-            getValue: getText,
+            onChange: (cb: () => unknown) => abstractOnChange(defectibleProps, system.getStatus, cb),
+            getValue: system.getStatus,
         };
         const statusBlade = addTextBlade(standardRowApi.getPane(), statusProp, { width: '60px' }, panelCleanup.add);
         statusBlade.element.classList.add('status', 'tp-rotv'); // This allows overriding tweakpane theme for this folder
         const applyThemeToStatus = () => (statusBlade.element.dataset.status = system.getStatus()); // this will change tweakpane theme for this folder, see tweakpane.css
-        panelCleanup.add(abstractOnChange(defectibleProps, system.getStatus, applyThemeToStatus));
+        panelCleanup.add(statusProp.onChange(applyThemeToStatus));
         applyThemeToStatus();
 
-        const heatProp = readProp<number>(shipDriver, `${system.pointer}/heat`);
-        const heatBlade = addTextBlade(
-            standardRowApi.getPane(),
-            heatProp,
-            { format: (heat: number) => `${Math.round(heat)}`, width: '60px' },
-            panelCleanup.add
-        );
+        const heatProp = {
+            onChange: (cb: () => unknown) =>
+                abstractOnChange([readProp<number>(shipDriver, `${system.pointer}/heat`)], system.getHeatStatus, cb),
+            getValue: system.getHeatStatus,
+        };
+        const heatBlade = addTextBlade(standardRowApi.getPane(), heatProp, { width: '60px' }, panelCleanup.add);
         heatBlade.element.classList.add('heat', 'tp-rotv'); // This allows overriding tweakpane theme for this folder
         const applyThemeToHeat = () => (heatBlade.element.dataset.status = system.getHeatStatus()); // this will change tweakpane theme for this folder, see tweakpane.css
-        panelCleanup.add(abstractOnChange([heatProp], system.getHeatStatus, applyThemeToHeat));
+        panelCleanup.add(heatProp.onChange(applyThemeToHeat));
         applyThemeToHeat();
     }
 }
