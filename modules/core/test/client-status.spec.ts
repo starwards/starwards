956634190632fbbc47b02e9ec3b3c9f56b397a93
Amir Arad <greenshade@gmail.com>
2024-03-18 18:35:11 +0200

    update error message expected when node-red  not connected

--- modules/core/test/client-status.spec.ts
+++ modules/core/test/client-status.spec.ts
@@ -20,7 +20,7 @@ describe('client status', () => {
                 status.getStatus,
                 expect.objectContaining({
                     status: Status.DISCONNECTED,
-                    text: expect.stringContaining('ECONNREFUSED') as unknown,
+                    text: expect.stringContaining('offline') as unknown,
                 }) as StatusInfo,
             );
         });
